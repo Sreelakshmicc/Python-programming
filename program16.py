d303309d0f6bb0a1b120e0a210cd4bac6aa219b9
Sreelakshmicc <sreelakshmicth@gmail.com>
2024-10-18 11:56:13 +0530

    Add files via upload

---
+++ program16.py
@@ -0,0 +1,10 @@
+def power(base,exponent):
+	if exponent==0:
+		return 1
+	else:
+		return base*power(base,exponent-1)
+base=float(input("Enter the value of base:"))
+exponent=int(input("Enter the value of exponent:"))
+result=power(base,exponent)
+print("Result is:",result)
+			
---
+++ program17.py
@@ -0,0 +1,6 @@
+string=input("Enter any string:")
+count=0
+for char in string:
+	if char=='a':
+		count+=1
+print("The number of occurences of 'a' in the string is:",count)		
