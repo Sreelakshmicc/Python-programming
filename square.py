d78ad6704e1c8a778971b5d3dc7a726d4556b237
Sreelakshmicc <sreelakshmicth@gmail.com>
2024-11-29 12:04:11 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,6 @@
+input_str = input("Enter the list elements:\n")   
+list_num = input_str.split()  
+list_num = [int(num) for num in list_num]  
+for i, v in enumerate(list_num):
+	list_num[i] = v**2
+print(list_num)
